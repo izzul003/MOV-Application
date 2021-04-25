36290ae8d48052ee71f749d5d3630d102ac16627
izzul003 <izzulislam03@gmail.com>
2021-04-25 17:23:42 +0700

    layouting splash, onboarding, login, signup, homescreen

---
+++ app/src/main/java/com/example/mov_application/onBoarding/OnboardingOneScreenActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mov_application.onBoarding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OnboardingOneScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding_one_screen)
+    }
+}
---
+++ app/src/main/java/com/example/mov_application/onBoarding/OnboardingThreeScreenActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mov_application.onBoarding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OnboardingThreeScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding_three_screen)
+    }
+}
